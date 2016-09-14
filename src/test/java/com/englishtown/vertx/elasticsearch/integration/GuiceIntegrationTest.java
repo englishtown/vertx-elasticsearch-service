291f094c88fcd20f7a2830c5c6f23b798040b4a9
Emir Dizdarevic <ed@hubrick.com>
2016-09-14 16:38:08 +0200

    * Improved tests

--- src/test/java/com/englishtown/vertx/elasticsearch/integration/GuiceIntegrationTest.java
+++ src/test/java/com/englishtown/vertx/elasticsearch/integration/GuiceIntegrationTest.java
@@ -1,17 +1,21 @@
 package com.englishtown.vertx.elasticsearch.integration;
 
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Guice integration tests
  */
 public class GuiceIntegrationTest extends IntegrationTestBase {
     @Override
-    protected void deployVerticle() throws Exception {
+    protected String getVerticleName() {
+        return "java-guice:com.englishtown.vertx.elasticsearch.ElasticSearchServiceVerticle";
+    }
+
+    @Override
+    protected void configure(JsonObject config) {
         config.put("address", "et.elasticsearch")
                 .put("guice_binder", new JsonArray()
                         .add("com.englishtown.vertx.elasticsearch.guice.ElasticSearchBinder"));
-
-        deployVerticle("java-guice:com.englishtown.vertx.elasticsearch.ElasticSearchServiceVerticle");
     }
 }
--- src/test/java/com/englishtown/vertx/elasticsearch/integration/ServiceIntegrationTest.java
+++ src/test/java/com/englishtown/vertx/elasticsearch/integration/ServiceIntegrationTest.java
@@ -1,11 +1,19 @@
 package com.englishtown.vertx.elasticsearch.integration;
 
+import io.vertx.core.json.JsonObject;
+
 /**
  * "service" prefix integration tests
  */
 public class ServiceIntegrationTest extends IntegrationTestBase {
+
     @Override
-    protected void deployVerticle() throws Exception {
-        deployVerticle("service:com.englishtown.vertx.vertx-elasticsearch-service");
+    protected String getVerticleName() {
+        return "service:com.englishtown.vertx.vertx-elasticsearch-service";
+    }
+
+    @Override
+    protected void configure(JsonObject config) {
+        // Do nothing
     }
 }
